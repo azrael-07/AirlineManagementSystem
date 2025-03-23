e30e52fea054229ccf4b74923ab1be149314202b
azrael-07 <gk1729@outlook.com>
2025-03-23 18:04:38 -0400

    Booking Backend From Form

--- airline_management/settings.py
+++ airline_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import pymysql
+import os
 pymysql.install_as_MySQLdb()
 
 DATABASES = {
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 STATICFILES_DIRS = [
     BASE_DIR / "apps/core/static",  # Add core static files
 ]
