cd030b6c3888892519a177dcc65162e6be4cce46
azrael-07 <gk1729@outlook.com>
2025-03-30 15:19:50 -0400

    stach

--- airline_management/settings.py
+++ airline_management/settings.py
@@ -90,10 +90,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+   
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'airline_management.urls'
 
